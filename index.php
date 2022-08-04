f73bdbe7ac2c7577a9bb0e119f26cda0e1e9ca32
Vipin Kumar <vipin.kumar@stellarinfo.com>
2022-08-04 16:24:52 +0530

    test

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $sqlConn =  new mysqli('localhost', 'root', '', 'mims_new');
  
  $result = $sqlConn->query('SELECT * FROM users'); 
- $resultArray = $result->fetch_all(MYSQLI_ASSOC);
+ $resultArray = $result->fetch_all(MYSQLI_ASSOC);   
  echo"<pre>";print_r($resultArray);
 
 ?>
